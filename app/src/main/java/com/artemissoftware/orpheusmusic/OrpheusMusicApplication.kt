438c7b82e49a07ca42710a17fa320df56458b27e
gustavo <gustavo.isaac@gmail.com>
2021-05-13 12:44:17 +0100

    Dagger-Hilt Setup

---
+++ app/src/main/java/com/artemissoftware/orpheusmusic/OrpheusMusicApplication.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.orpheusmusic
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class OrpheusMusicApplication :Application()
