e57c5d292669a2d02dbefa329ef8de3f78347ecb
gustavo <gustavo.isaac@gmail.com>
2021-05-21 20:41:56 +0100

    MainViewModel

--- app/src/main/java/com/artemissoftware/orpheusmusic/di/AppModule.kt
+++ app/src/main/java/com/artemissoftware/orpheusmusic/di/AppModule.kt
@@ -2,6 +2,7 @@ package com.artemissoftware.orpheusmusic.di
 
 import android.content.Context
 import com.artemissoftware.orpheusmusic.R
+import com.artemissoftware.orpheusmusic.exoplayer.MusicServiceConnection
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
@@ -15,6 +16,13 @@ import javax.inject.Singleton
 @Module
 @InstallIn(ApplicationComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideMusicServiceConnection(
+        @ApplicationContext context: Context
+    ) = MusicServiceConnection(context)
+
 
     @Singleton
     @Provides
